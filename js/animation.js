43131987418667ec39748bbe70beb6d192ffe5fe
reedleaf <1020273485@qq.com>
2023-03-25 14:14:54 +0800

    Site updated: 2023-03-25 14:14:54

--- js/animation.js
+++ js/animation.js
@@ -29,7 +29,7 @@
     setTimeout(() => {
         $('body > .navbar, body > .section, body > .footer').forEach(element => {
             element.style.opacity = '1';
-            element.style.transition = 'opacity 0.3s ease-out, transform 0.3s ease-out';
+            element.style.transition = 'opacity 0.3s ease-out, transform 0.3s ease-out, box-shadow 0.3s ease-in-out';
         });
         document.querySelector('body > .navbar').style.transform = 'translateY(0)';
 
@@ -44,7 +44,7 @@
                     element.style.opacity = '1';
                     element.style.transform = '';
                     element.style.transition = 'opacity 0.3s ease-out, transform 0.3s ease-out';
-                }, i * 100);
+                }, i * 30);
                 i++;
             });
         });
